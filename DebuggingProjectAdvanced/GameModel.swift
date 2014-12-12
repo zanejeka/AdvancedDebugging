bddcb8fe829cca07108f43591f968a7203eae66a
craiggrummitt <craiggrummitt@gmail.com>
2014-12-12 13:50:25 -0500

    Set up game

---
+++ DebuggingProjectAdvanced/GameModel.swift
@@ -0,0 +1,33 @@
+//
+//  GameModel.swift
+//  DebuggingProjectAdvanced
+//
+//  Created by Craig on 12/12/2014.
+//  Copyright (c) 2014 InteractiveCoconut. All rights reserved.
+//
+
+import Foundation
+
+class GameModel {
+    var computerRandomNumber:Int
+    init() {
+        computerRandomNumber = GameModel.getRandomNumber()
+    }
+    func resetComputerRandomNumber() {
+        computerRandomNumber = GameModel.getRandomNumber()
+    }
+    //returns correct, is higher
+    func makeGuess(userGuess:Int)->(Bool,Bool) {
+        if (computerRandomNumber == userGuess) {
+            return(true,false)
+        } else {
+            let isHigher = userGuess > computerRandomNumber
+            return(false,isHigher)
+        }
+    }
+    //MARK: class function
+    class func getRandomNumber() -> Int {
+        let randomNumber = Int(arc4random_uniform(10)) + 1
+        return randomNumber
+    }
+}
--- DebuggingProjectAdvanced/ViewController.swift
+++ DebuggingProjectAdvanced/ViewController.swift
@@ -8,18 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    let gameModel = GameModel()
+    
+    @IBOutlet weak var guessTextField: UITextField!
+    @IBOutlet weak var guessFeedback: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var instructionsLabel: UILabel!
+    @IBOutlet weak var instructionsLabel2: UILabel!
+    @IBOutlet weak var guessButton: UIButton!
+    
+    var tries = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        guessTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func checkGuess() {
+        var guess = guessTextField.text.toInt()
+        if let guessUnwrapped = guess {
+            tries++
+            //get tuple back from gameModel
+            var (guessIsCorrect,guessIsHigher) = gameModel.makeGuess(guessUnwrapped)
+            if (guessIsCorrect) {
+                guessFeedback.text = "\(guessUnwrapped)! Well done! You guessed it in \(tries) tries!"
+                //hide the game, and display play again button
+                playAgainButton.hidden = false
+                instructionsLabel.hidden = true
+                instructionsLabel2.hidden = true
+                guessTextField.hidden = true
+                guessButton.hidden = true
+            } else if (guessIsHigher) { //guess was higher, so the player should guess lower
+                guessFeedback.text = "Try \(tries) - \(guessUnwrapped): Bad luck! Lower!"
+            } else {                    //guess was lower, so the player should guess higher
+                guessFeedback.text = "Try \(tries) - \(guessUnwrapped): Bad luck! Higher!"
+            }
+        }
+        guessTextField.text = ""
+    }
+    
+    //MARK: UITextFieldDelegate functions
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        checkGuess()
+        return true
+    }
+    //MARK: IBActions
+    @IBAction func makeGuess(sender: AnyObject) {
+        guessTextField.resignFirstResponder()
+        checkGuess()
+    }
+    @IBAction func playAgain(sender: AnyObject) {
+        //reset the game
+        gameModel.resetComputerRandomNumber()
+        guessFeedback.text = ""
+        tries = 0
+        //hide the play again button, display the game
+        playAgainButton.hidden = true
+        guessTextField.hidden = false
+        guessButton.hidden = false
+        instructionsLabel.hidden = false
+        instructionsLabel2.hidden = false
+        
+    }
 }
 
